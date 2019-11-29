944ffaeaef0e797350f29e79218ef314d3f1530e
Alexey Alexeyev <algruun@gmail.com>
2019-11-29 14:41:03 +0300

    added currencies token api to env

--- src/config.py
+++ src/config.py
@@ -4,3 +4,8 @@ HOST_NAME = (
     "localhost" if "HOST_NAME" not in os.environ else os.environ.get("HOST_NAME")
 )
 PORT = 8000 if "PORT" not in os.environ else os.environ.get("PORT")
+CURRENCIES_API_KEY = (
+    "b4eae1e78311f3a23590"
+    if "CURRENCIES_API_KEY" not in os.environ
+    else os.environ.get("CURRENCIES_API_KEY")
+)
